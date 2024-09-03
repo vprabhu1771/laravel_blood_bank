13b0aead90f146fde47e356b30301f3c80bb4b9e
krishna-432001 <krishnapriya432001@gmail.com>
2024-09-03 12:47:14 +0530

    Update login.blade.php
    bootstrap design added

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -1,71 +1,60 @@
 @extends('frontend.layout.app')
 
 @section('title')
- 
-login page
-
+    Login Page
 @endsection
 
 @section('content')
 
-<main class="main pages">
-    <div class="page-header breadcrumb-wrap">
-        <div class="container">
-            <div class="breadcrumb">
-                <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
-                <span></span> Pages <span></span> My Account
-            </div>
-        </div>
+<main class="container py-5">
+    <div class="page-header mb-4">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('/') }}"><i class="fi-rs-home mr-2"></i>Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Pages</li>
+                <li class="breadcrumb-item active" aria-current="page">My Account</li>
+            </ol>
+        </nav>
     </div>
-    <div class="page-content pt-150 pb-150">
-        <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
             <div class="row">
-                <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
-                    <div class="row">
-                        <div class="col-lg-6 pr-30 d-none d-lg-block">
-                            <img class="border-radius-15" src="frontend/imgs/page/login-1.png" alt="" />
-                        </div>
-                        <div class="col-lg-6 col-md-8">
-                            <div class="login_wrap widget-taber-content background-white">
-                                <div class="padding_eight_all bg-white">
-                                    <div class="heading_s1">
-                                        <h1 class="mb-5">Login</h1>
-                                        <p class="mb-30">Don't have an account? <a href="page-register.html">Create here</a></p>
-                                    </div>
-                                    <form action="/authenticate" method="post">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="text" required="" name="email" placeholder="Username or Email *" />
-                                        </div>
-                                        <div class="form-group">
-                                            <input required="" type="password" name="password" placeholder="Your password *" />
-                                        </div>
-                                        <div class="login_footer form-group">
-                                            <div class="chek-form">
-                                                <input type="text"  name="captcha" placeholder="Security code *" />
-                                            </div>
-                                            <span class="security-code">
-                                                <b class="text-new">8</b>
-                                                <b class="text-hot">6</b>
-                                                <b class="text-sale">7</b>
-                                                <b class="text-best">5</b>
-                                            </span>
-                                        </div>
-                                        <div class="login_footer form-group mb-50">
-                                            <div class="chek-form">
-                                                <div class="custome-checkbox">
-                                                    <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox1" value="" />
-                                                    <label class="form-check-label" for="exampleCheckbox1"><span>Remember me</span></label>
-                                                </div>
-                                            </div>
-                                            <a class="text-muted" href="route('home.forget_password')">Forgot password?</a>
-                                        </div>
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-heading btn-block hover-up" name="login">Log in</button>
-                                        </div>
-                                    </form>
+                <div class="col-lg-6 d-none d-lg-block">
+                    <img class="img-fluid rounded" src="{{ asset('frontend/imgs/page/login-1.png') }}" alt="Login Image">
+                </div>
+                <div class="col-lg-6">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h1 class="h4 mb-4">Login</h1>
+                            <p class="mb-4">Don't have an account? <a href="{{ route('home.register') }}">Create here</a></p>
+                            <form action="/authenticate" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" class="form-control" required name="email" placeholder="Username or Email *">
                                 </div>
-                            </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control" required name="password" placeholder="Your password *">
+                                </div>
+                                <div class="form-group">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <input type="text" class="form-control" name="captcha" placeholder="Security code *">
+                                        <span class="security-code ml-2">
+                                            <b class="text-primary">8</b>
+                                            <b class="text-danger">6</b>
+                                            <b class="text-warning">7</b>
+                                            <b class="text-success">5</b>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="form-group d-flex justify-content-between align-items-center mb-4">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" name="checkbox" id="rememberMe">
+                                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                                    </div>
+                                    <a class="text-muted" href="{{ route('home.forget_password') }}">Forgot password?</a>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block">Log in</button>
+                            </form>
                         </div>
                     </div>
                 </div>
