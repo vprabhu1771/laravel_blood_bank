4a0176764f2a5a50259f1529055df188643c4eca
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-06 17:46:38 +0530

    Update topbar.blade.php

--- resources/views/frontend/layout/topbar.blade.php
+++ resources/views/frontend/layout/topbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Small Shop</a>
+    <a class="navbar-brand" href="#">Blood Bank</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
