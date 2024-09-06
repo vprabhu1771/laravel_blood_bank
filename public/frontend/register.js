4d7e05bf9056ff832849f4798fc2af21ebc3540f
krishna-432001 <krishnapriya432001@gmail.com>
2024-09-06 13:28:34 +0530

    Update register.js
    added fetch email validation and password validation

--- public/frontend/register.js
+++ public/frontend/register.js
@@ -2,6 +2,8 @@ window.onload = function()
 {
     console.log('document loaded');
     fetch_name();
+    fetch_email();
+    fetch_password();
     fetch_pincode();
 }
 
@@ -12,7 +14,76 @@ function fetch_name()
     });
 }
 
+function fetch_email() {
+    $('#email').on('input', function() {
+        var email = $(this).val();
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+        if (emailRegex.test(email)) {
+            // Email is valid, check its availability
+            $(this).removeClass('is-invalid').addClass('is-valid');
+            $('#emailError').text('');
 
+            // AJAX request to check email availability
+            $.ajax({
+                url: '/check-email', // Your API endpoint
+                method: 'GET',
+                data: { email: email },
+                success: function(response) {
+                    // Email is available
+                    $('#emailError').text('');
+                    $('#email').removeClass('is-invalid').addClass('is-valid');
+                },
+                error: function(xhr) {
+                    // Email is not available
+                    if (xhr.status === 409) {
+                        $('#emailError').text('Email is already taken');
+                        $('#email').removeClass('is-valid').addClass('is-invalid');
+                    } else {
+                        // Handle other errors
+                        $('#emailError').text('An error occurred. Please try again.');
+                        $('#email').removeClass('is-valid').addClass('is-invalid');
+                    }
+                }
+            });
+
+        } else {
+            // Email is invalid
+            $(this).removeClass('is-valid').addClass('is-invalid');
+            $('#emailError').text('Please enter a valid email address');
+        }
+    });
+}
+
+
+function fetch_password()
+{
+    $('#confirmPassword').on('input', function() {
+        var password = $('#password').val();
+        var confirmPassword = $(this).val();
+
+        if (password === confirmPassword) {
+            $(this).removeClass('is-invalid').addClass('is-valid');
+            $('#passwordError').text('');
+        } else {
+            $(this).removeClass('is-valid').addClass('is-invalid');
+            $('#passwordError').text('Passwords do not match');
+        }
+    });
+
+    $('#password').on('input', function() {
+        var password = $(this).val();
+        var confirmPassword = $('#confirmPassword').val();
+
+        if (password === confirmPassword) {
+            $('#confirmPassword').removeClass('is-invalid').addClass('is-valid');
+            $('#passwordError').text('');
+        } else {
+            $('#confirmPassword').removeClass('is-valid').addClass('is-invalid');
+            $('#passwordError').text('Passwords do not match');
+        }
+    });
+}
 
 function fetch_pincode()
 {
