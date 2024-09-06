1732edb6e767c4be704370be836af5c858acbc72
krishna-432001 <krishnapriya432001@gmail.com>
2024-09-06 13:00:08 +0530

    Update register.blade.php
    area district form added and disabled added

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -68,27 +68,38 @@
                                     <label for="phone">Phone</label>
                                     <input type="text" class="form-control" required name="phone" id="phone" placeholder="Phone">
                                 </div>
+
+                                <div class="form-group">
+                                    <label for="address">Address</label>
+                            
+                                    <textarea  class="form-control" required name="address" id="address"></textarea>
+                                </div>
                                 <div class="form-group">
                                     <label for="pincode">Pincode</label>
                                     <input type="text" class="form-control" required name="pincode" id="pincode" placeholder="Pincode">
                                 </div>
                                 <div class="form-group">
+                                    <label for="city">Area</label>
+                                    <input type="text" class="form-control" required name="area" id="area" placeholder="area" disabled>
+                                </div>
+                                <div class="form-group">
+                                    <label for="city">District</label>
+                                    <input type="text" class="form-control" required name="district" id="district" placeholder="district" disabled>
+                                </div>
+                                <div class="form-group">
                                     <label for="city">City</label>
-                                    <input type="text" class="form-control" required name="city" id="city" placeholder="city">
+                                    <input type="text" class="form-control" required name="city" id="city" placeholder="city" disabled>
                                 </div>
                                 <div class="form-group">
                                     <label for="state">State</label>
-                                    <input type="text" class="form-control" required name="state" id="state" placeholder="state">
+                                    <input type="text" class="form-control" required name="state" id="state" placeholder="state" disabled>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="country">Country</label>
-                                    <input type="text" class="form-control" required name="country" id="country" placeholder="Country">
+                                    <input type="text" class="form-control" required name="country" id="country" placeholder="Country" disabled>
                                 </div>
-                                <div class="form-group">
-                                    <label for="address">Address</label>
-                                    <input type="text" class="form-control" required name="address" id="address" placeholder="Address">
-                                </div>
+                               
                              
                                 <div class="form-group mb-4">
                                     <label for="captcha">Security Code</label>
