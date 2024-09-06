e5512d7dfa105db1f6f30b7a17c3982dfe0daff1
krishna-432001 <krishnapriya432001@gmail.com>
2024-09-06 12:21:34 +0530

    navigation
    navigation group added  logic

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -19,6 +19,10 @@ class CityResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Location';
+
+    protected static ?int $navigationSort = 3;
+
     public static function form(Form $form): Form
     {
         return $form
--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -19,6 +19,10 @@ class StateResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Location';
+
+    protected static ?int $navigationSort = 2;
+
     public static function form(Form $form): Form
     {
         return $form
